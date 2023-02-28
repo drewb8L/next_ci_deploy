06effbcfac88f37d5e18dc65f23003bb3eac8edf
drewb8L <104442990+drewb8L@users.noreply.github.com>
2023-02-28 10:21:43 -0600

    Added MyButton component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import {MyButton} from "@/components/MyButton";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
         <h1>
           Hello World
         </h1>
+          <MyButton/>
       </div>
   )
 }
